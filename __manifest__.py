966945ab8043bc21c34cd87570905f769e68c9b9
L3N1N <len.pardo1966@gmail.com>
2025-12-09 12:18:56 -0500

    fix: Odoo 18 XML schema, mail template and QWeb report

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,18 @@
 {
     'name': 'Helpdesk Firma (Enterprise)',
-    'summary': 'Firma digital y envío de tickets con PDF tipo factura',
-    'description': """
-Extiende Helpdesk para permitir:
-
-- Firma digital del ticket desde el formulario
-- Generación de PDF firmado
-- Envío del ticket por correo con PDF adjunto
-- Integración con Odoo Sign (Enterprise)
-
-Diseñado para Odoo 18 Enterprise
-""",
+    
     'version': '18.0.1.1.0',
+    'summary': 'Firma digital y envío de tickets con PDF',
     'author': 'Lenin Chela',
     'maintainer': 'Lenin Chela',
     'website': 'https://github.com/lenin1996/helpdesk-firma',
     'license': 'OPL-1',
     'category': 'Helpdesk',
-    'depends': ['mail','helpdesk','sign',],
+    'depends': [
+        'mail',
+        'helpdesk',
+        'sign',
+    ],
     'data': [
         'security/ir.model.access.csv',
         'data/mail_template.xml',
@@ -25,6 +20,8 @@ Diseñado para Odoo 18 Enterprise
         'wizard/envio_wizard_views.xml',
         'wizard/firma_wizard_views.xml',
         'report/report_ticket_firma.xml',
+    ],
+    'qweb': [
         'report/ticket_firma_template.xml',
     ],
     'installable': True,
